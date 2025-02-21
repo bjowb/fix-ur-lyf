f5572219e6a7fe4afb2ad22127db6071e7fd9800
haibaaa <9700samarth@gmail.com>
2025-02-21 11:33:03 +0530

    navbar color

--- frontend/fix-ur-lyf/src/components/taskview.svelte
+++ frontend/fix-ur-lyf/src/components/taskview.svelte
@@ -1,4 +1,4 @@
-<div class="flex-1 flex flex-col items-center justify-center text-center text-white bg-gray-800 h-screen">
+<div class="flex-1 flex flex-col items-center justify-center text-center text-white bg-[#323232] h-screen">
     <img src="/empty.svg" alt="No Tasks" class="w-48 opacity-50">
     <p class="mt-4 text-lg font-semibold">All lists are hidden</p>
     <p class="text-gray-400">Select any list to see your tasks</p>
