430df7f6f58b5a4d645db06786bc706112a88976
haibaaa <9700samarth@gmail.com>
2025-02-22 12:17:49 +0530

    nav

--- frontend/alt/src/components/nav.svelte
+++ frontend/alt/src/components/nav.svelte
@@ -6,5 +6,14 @@
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block h-5 w-5 stroke-current"> <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path> </svg>
         </button>
     </div>
-    <button class="btn btn-soft btn-success">fix-ur-lyf</button>
+
+    <button class="btn btn-link no-underline hover:no-underline text-[#00ADB5] text-[24px]">fix-ur-lyf</button>
+    
+    <div class = "ml-auto flex gap-2 mr-4">
+      <div class="avatar">
+        <div class="w-10 rounded-full">
+          <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+        </div>
+      </div>
+    </div>
 </div>
